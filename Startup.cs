3dd4b7782459bcd7a21e4a66be4ff16766ec298c
Enkeli __ <programmistik@hotmail.com>
2021-09-10 17:41:45 +0400

    --

--- Startup.cs
+++ Startup.cs
@@ -47,14 +47,14 @@ namespace ProfileAPI
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
+            //if (env.IsDevelopment())
+            //{
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "ProfileAPI v1"));
-            }
+            //}
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
 
             app.UseRouting();
 
